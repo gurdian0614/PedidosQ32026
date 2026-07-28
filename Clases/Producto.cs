b0401530de00756c2f6052f64ad2152dbcae8708
gurdian0614 <ivan.gurdian@outlook.com>
2026-07-27 19:19:29 -0600

    Create Producto class

---
+++ Clases/Producto.cs
@@ -0,0 +1,29 @@
+public class Producto
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; } // ?: La Propiedad acepta valores nulos
+    public decimal Precio {get; set; }
+    public int Stock { get; set;}
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("******************** DATOS DEL PRODUCTO ********************");
+        Console.WriteLine($"ID: {Id}");
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Precio: {Precio}");
+        Console.WriteLine($"Cantidad Disponible: {Stock}");
+        Console.WriteLine();
+    }
+
+    public void DisminuirStock(int Cantidad)
+    {
+        if (Stock >= Cantidad)
+        {
+            // Equivalente a Stock = Stock - Cantidad
+            Stock -= Cantidad;
+        } else
+        {
+            Console.WriteLine($"No hay suficiente stock de {Nombre}");
+        }
+    }
+}
