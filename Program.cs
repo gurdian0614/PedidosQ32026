93d01e7e8db4fc778f385cc1e273bc861ec252d0
gurdian0614 <ivan.gurdian@outlook.com>
2026-07-27 19:19:54 -0600

    Adding first data

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Producto producto1 = new Producto();
+producto1.Id = 1;
+producto1.Nombre = "Mouse";
+producto1.Precio = 250.00m;
+producto1.Stock = 50;
+producto1.MostrarInformacion();
+
+Producto producto2 = new Producto();
+producto2.Id = 2;
+producto2.Nombre = "Teclado";
+producto2.Precio = 500.00m;
+producto2.Stock = 30;
+producto2.MostrarInformacion();
