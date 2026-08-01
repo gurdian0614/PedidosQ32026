b650652bd4897905381e6648792d3c1ea39f5070
gurdian0614 <ivan.gurdian@outlook.com>
2026-07-31 19:15:55 -0600

    Adding derived classes

---
+++ Clases/Libro.cs
@@ -0,0 +1,15 @@
+public class Libro : Producto
+{
+    public string? ISBN { get; set; }
+    public string? Autor { get; set; }
+    public int NumeroPaginas { get; set; }
+
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"ISBN: {ISBN}");
+        Console.WriteLine($"Autor: {Autor}");
+        Console.WriteLine($"Número de Páginas: {NumeroPaginas}");
+        Console.WriteLine();
+    }
+}
