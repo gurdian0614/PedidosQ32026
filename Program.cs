8ef638b10ae0111df4feae96953894d3ec6017f8
gurdian0614 <ivan.gurdian@outlook.com>
2026-08-03 18:58:42 -0600

    Fix data

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
-﻿Producto producto1 = new Producto();
-producto1.Id = 1;
-producto1.Nombre = "Mouse";
-producto1.Precio = 250.00m;
-producto1.Stock = 50;
-producto1.MostrarInformacion();
+﻿Electronico electronico2 = new Electronico();
+electronico2.Id = 1;
+electronico2.Nombre = "Mouse";
+electronico2.Precio = 250.00m;
+electronico2.Stock = 50;
+electronico2.GarantiaMeses = 6;
+electronico2.Voltaje = "110V";
+electronico2.MostrarInformacion();
 
-Producto producto2 = new Producto();
-producto2.Id = 2;
-producto2.Nombre = "Teclado";
-producto2.Precio = 500.00m;
-producto2.Stock = 30;
-producto2.MostrarInformacion();
+Electronico electronico3 = new Electronico();
+electronico3.Id = 2;
+electronico3.Nombre = "Teclado";
+electronico3.Precio = 500.00m;
+electronico3.Stock = 30;
+electronico3.GarantiaMeses = 12;
+electronico3.Voltaje = "110V";
+electronico3.MostrarInformacion();
 
 Pedido pedido1 = new Pedido(1);
-pedido1.AgregarItem(producto1, 45);
-pedido1.AgregarItem(producto2, 10);
+pedido1.AgregarItem(electronico2, 45);
+pedido1.AgregarItem(electronico3, 10);
 pedido1.MostrarDetalles();
 
 Electronico electronico = new Electronico();
@@ -42,7 +46,7 @@ pedido2.AgregarItem(libro, 25);
 pedido2.MostrarDetalles();
 
 Console.WriteLine("****************************** ACTUALIZACIÓN DE STOCK ******************************");
-producto1.MostrarInformacion();
-producto2.MostrarInformacion();
+electronico2.MostrarInformacion();
+electronico3.MostrarInformacion();
 electronico.MostrarInformacion();
 libro.MostrarInformacion();
