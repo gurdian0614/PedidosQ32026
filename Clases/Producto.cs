8c8488cdb0d603eeb06a27bba914a072dbae5ecf
gurdian0614 <ivan.gurdian@outlook.com>
2026-08-03 18:58:19 -0600

    Create abstract class

--- Clases/Producto.cs
+++ Clases/Producto.cs
@@ -1,19 +1,12 @@
-public class Producto
+public abstract class Producto
 {
     public int Id { get; set; }
     public string? Nombre { get; set; } // ?: La Propiedad acepta valores nulos
     public decimal Precio {get; set; }
     public int Stock { get; set;}
 
-    public virtual void MostrarInformacion()
-    {
-        Console.WriteLine("******************** DATOS DEL PRODUCTO ********************");
-        Console.WriteLine($"ID: {Id}");
-        Console.WriteLine($"Nombre: {Nombre}");
-        Console.WriteLine($"Precio: {Precio}");
-        Console.WriteLine($"Cantidad Disponible: {Stock}");
-    }
-
+    public abstract void MostrarInformacion();
+    
     public void DisminuirStock(int Cantidad)
     {
         if (Stock >= Cantidad)
